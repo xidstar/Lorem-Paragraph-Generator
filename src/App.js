5890826489448ecd30dbde82215c401ee9483a65
Sidney Oluoch <xidstar@gmail.com>
2021-08-12 14:40:38 -0400

    Paragraph Generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 
 function App() {
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count)
+    if (amount <= 0) {
+      amount = 1;
+    }
+
+    if (amount > 5) {
+      amount = 5;
+    }
+    setText(data.slice(0, amount));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="lorem-container">
+      <h1>Tired of boring ipsum?</h1>
+      <form className="lorem-input" onSubmit={handleSubmit}>
+        <h3>Paragraphs</h3>
+        <input type="number" name="amount" className="" value={count} onChange={(e) => setCount(e.target.value)} />
+        <button type="submit" className="lorem-btn" >Generate</button>
+      </form>
+      <div className="lorem-text">
+        {text.map((item, index) => {
+          return (
+            <p key={index}>{item}</p>
+          )
+        })}
+      </div>
+    </section>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,11 @@
+const paragraphs = [
+
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laboriosam amet quod est voluptatem nihil vel, sit architecto atque reprehenderit officia similique dolorem iure ex, id ipsam consequatur facere fugit.",
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora ratione, nobis et, excepturi consequatur quae neque aliquid nostrum error aut, doloremque sapiente. Voluptatem mollitia adipisci delectus natus inventore ducimus minima corporis magnam debitis commodi? Nisi hic, sint tempora voluptatum praesentium perferendis fugit mollitia? Laboriosam quae qui commodi incidunt sit reprehenderit vitae hic iure perferendis cum a, itaque error impedit eveniet! ',
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt optio sit reprehenderit quos deserunt consectetur nobis commodi, veritatis hic nulla minima ipsam culpa sint reiciendis magni ullam quam itaque, provident fugit maiores pariatur! Cumque temporibus culpa voluptatum voluptatibus exercitationem id, sunt obcaecati blanditiis. Magni.',
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, perspiciatis amet sed autem veniam assumenda quibusdam neque, modi accusantium quam molestias! Hic, officia obcaecati excepturi blanditiis repudiandae quidem itaque fugiat? At molestias modi nostrum praesentium error maxime obcaecati quos alias!',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium a, non atque dolore vero dolor possimus natus enim id, aut repellendus, exercitationem dolorem vitae. Animi odit necessitatibus saepe magnam eum.'
+
+]
+
+export default paragraphs
